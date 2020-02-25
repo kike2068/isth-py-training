d3f70b4a099ad3d2c008c189aeb1c2440a368803
kike2068 <61214788+kike2068@users.noreply.github.com>
2020-02-25 14:59:23 -0600

    Update file_repository.py

--- app/infrastructure/file_repository.py
+++ app/infrastructure/file_repository.py
@@ -1,10 +1,22 @@
 """Service methods for file endpoints"""
 
+from app.common import Constants
+import os
 
 class fileRepository:
 
     def get_file(self, id):
         return NotImplementedError
 
-    def post_file(self, data):
+    def post_file(self, nombre, data):
         return NotImplementedError
+
+        filepath = os.path.join(FILE_UPLOAD_PATH, nombre)
+
+        if not os.path.exists(FILE_UPLOAD_PATH):
+            os.makedirs(FILE_UPLOAD_PATH)
+
+        f = open(filepath, "a")
+
+        prueba = data.provide()
+        
